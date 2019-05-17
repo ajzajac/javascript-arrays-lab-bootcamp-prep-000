341d1cd7001010d85df2c14ced86cc241211c037
Andrew Zajac <ajzajac@icloud.com>
2019-05-17 18:33:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push('Ralph')
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift('Bob')
+}
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift()
+}
+function appendKitten(name){
+  return kittens.concat('Broom')
+}
+function prependKitten(name){
+  return kittens.slice()
+}
